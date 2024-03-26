75b3b17b94a057340137f8b2623294ac44eb8e1c
Akihito Tamagawa <tama3bb@gmail.com>
2024-03-26 03:38:05 +0000

    gpt-4-turbo-preview as openai_model

--- python/api-examples-source/chat.llm.py
+++ python/api-examples-source/chat.llm.py
@@ -13,7 +13,7 @@ with st.expander("ℹ️ Disclaimer"):
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state["openai_model"] = "gpt-4-turbo-preview"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
